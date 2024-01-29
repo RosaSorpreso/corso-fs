eba9f407b2a61c538a66fe87e3ff4ebcd09577a9
RosaSorpreso <rosasorpreso@gmail.com>
2024-01-29 18:04:34 +0100

    correzione esercizio g1

--- unit 1/s2/g1/js/D1.js
+++ unit 1/s2/g1/js/D1.js
@@ -69,5 +69,4 @@ console.log(x)
 let name1 = 'john'
 let name2 = 'John'
 console.log(name1 == name2)
-name2 = name2.toLowerCase()
-console.log(name1 == name2)
+console.log(name1 == name2.toLowerCase())
