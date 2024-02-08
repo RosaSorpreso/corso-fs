95f71631a7d3694bb042c8838d9c14a7024f1e5f
RosaSorpreso <rosasorpreso@gmail.com>
2024-02-08 23:34:06 +0100

    esercizio g4

---
+++ unit 1/s3/g4/js/script.js
@@ -0,0 +1,9 @@
+let tabellone = document.querySelector('.tabellone')
+let button = document.querySelector('.button')
+
+for(i = 1; i <= 76; i++){
+    let numbers = document.createElement('div')
+    numbers.classList.add('numbers')
+    numbers.innerText = i
+    tabellone.append(numbers)
+}
